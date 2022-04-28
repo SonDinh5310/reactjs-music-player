0e9166a6f4b602a3d33fdf4030e1c5e82679de42
Sondinh <dnson20022001@gmail.com>
2022-04-28 11:16:39 +0700

    update styling

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Loading() {
     return (
-        <div className="w-full mt-20">
-            <p className="text-center text-2xl">Loading...</p>
+        <div className="w-full h-full mt-20">
+            <p className="text-center text-2xl animate-ping text-slate-200">
+                LOADING...
+            </p>
         </div>
     );
 }
